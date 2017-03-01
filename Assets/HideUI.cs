43eb6f168bd1218b3dbbaa78aa2ef16c6baad8e0
lordcheetah <lordcheetah@comcast.net>
2017-02-28 19:09:28 -0500

    feat: Initial commit
    This shows the museum and the welcome screen

---
+++ Assets/HideUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(RemoveAfterSeconds(10));
+	}
+
+	IEnumerator RemoveAfterSeconds(int seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		gameObject.SetActive(false);
+	}
+}
